16218a4e53560e62c33f5d8cc60a9ec3b6620061
kurage0516 <73592225+kurage0516@users.noreply.github.com>
2021-01-10 10:32:44 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,62 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :move_to_index, except: :edit
+
+  def index
+    @prototype = Prototype.new
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  def show
+    @prototype = Prototype.new
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments
+  end
+
+  def edit
+    @prototype = Prototype.new
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype.id), method: :get
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    if @prototype.delete
+      redirect_to root_path
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+  
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.new
+    @user = User.find(params[:id])
+  end
+
+end
